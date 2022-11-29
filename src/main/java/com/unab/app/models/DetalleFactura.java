51c927b868cbda70d5c23a5bcee04d1531c2a70d
jhonnyer <jhonnyerg@unicauca.edu.co>
2022-11-28 21:51:40 -0500

    Entidades

--- src/main/java/com/unab/app/models/DetalleFactura.java
+++ src/main/java/com/unab/app/models/DetalleFactura.java
@@ -1,5 +1,61 @@
 package com.unab.app.models;
 
-public class DetalleFactura {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="detalleFactura")
+public class DetalleFactura implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="id_detalle_factura")
+	private Long id;
+	
+	@Column(name="cantidad")
+	private Integer cantidad;
+	
+	@ManyToOne()
+	@JoinColumn(name="factura_id_factura", referencedColumnName="id_factura")
+	private Factura factura;
+	
+	@ManyToOne()
+	@JoinColumn(name="producto_id_producto", referencedColumnName="id_producto")
+	private Producto producto;
+
+	public DetalleFactura() {
+	}
+
+	public DetalleFactura(Long id, Integer cantidad) {
+		this.id = id;
+		this.cantidad = cantidad;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(Integer cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
 }
--- src/main/java/com/unab/app/models/Producto.java
+++ src/main/java/com/unab/app/models/Producto.java
@@ -1,5 +1,80 @@
 package com.unab.app.models;
 
-public class Producto {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "producto")
+public class Producto implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_producto")
+	private Long id;
+
+	@Column(name = "nombre")
+	private String nombre;
+
+	@Column(name = "precio")
+	private String precio;
+
+	@Column(name = "create_at")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private String createAt;
+
+	public Producto() {
+	}
+
+	public Producto(Long id, String nombre, String precio, String createAt) {
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.createAt = createAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+	public String getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(String createAt) {
+		this.createAt = createAt;
+	}
 
 }
